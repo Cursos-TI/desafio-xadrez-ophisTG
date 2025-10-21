b6b5f6457dc52530c5ffab277d1c60598ee61cb8
Rafael Ribeiro <163664337+ophisTG@users.noreply.github.com>
2025-10-21 19:24:51 +0000

    Adiciona peça: Cavalo - loop aninhado

--- xadrez.c
+++ xadrez.c
@@ -4,13 +4,14 @@
 
 int main() {
     // Define e inicializa variaveis:
-    int opcao, bispo = 1, rainha = 1;
+    int opcao, bispo = 1, rainha = 1, cavalo = 0;
     
     // Imprime opções do jogador:
     printf("===== TABULEIRO =====\n");
     printf("[1] TORRE\n");
     printf("[2] BISPO\n");
     printf("[3] RAINHA\n");
+    printf("[4] CAVALO\n");
     printf("[0] SAIR\n");
     printf("=====================\n");
 
@@ -19,7 +20,7 @@ int main() {
     scanf("%d", &opcao);
 
     // Verifica se a opção escolhida é valida(apenas números inteiros.):
-    while(opcao < 0 || opcao > 3){
+    while(opcao < 0 || opcao > 4){
         printf("Opção inválida, tente novamente: ");
         scanf("%d", &opcao);
     }
@@ -29,17 +30,17 @@ int main() {
         case 1:
             //Movimenta a torre 5 casas para a direita:
             printf("-----------------------------\n");
+            printf("Torre se move... \n");
             for (int torre = 1; torre <=5; torre++){
-                printf("Torre se move para direita...\n");
-                printf("Direita...\n");
+                printf("Direita\n");
                 printf("%s: %d\n", (torre ==1) ? "Casa movida" : "Casas movidas", torre);
             }
             break;
         case 2:
             // Movimenta o bispo 5 casas na diagonal:
+            printf("Bispo se move...\n");
             printf("-----------------------------\n");
             while (bispo <= 5) {
-                printf("Bispo se move para diagonal direita acima: \n");
                 printf("Cima\n");
                 printf("Direita\n");
                 printf("%s: %d\n", (bispo ==1) ? "Casa movida" : "Casas movidas", bispo);
@@ -49,12 +50,29 @@ int main() {
         case 3:
             // Movimenta a rainha 8 casas para a esquerda
             printf("-----------------------------\n");
+            printf("Rainha se move...\n");
             do {
-                printf("Rainha se movimenta para esquerda: ");
-                printf("Esquerda...\n");
+                printf("Esquerda\n");
                 printf("%s: %d\n", (rainha ==1) ? "Casa movida" : "Casas movidas", rainha);
                 rainha++;
             } while (rainha <= 8);
+            break;
+        case 4:
+            // Movimenta o cavalo em L para baixo e para esquerda:
+            printf("-----------------------------\n");
+            printf("Cavalo se move...\n");
+            for (int i = 0; i < 2; i++){
+                int passos = 0;
+                while (passos < 1){
+                    printf("Baixo\n");
+                    cavalo++;
+                    printf("%s: %d\n", (cavalo ==1) ? "Casa movida" : "Casas movidas", cavalo);
+                    passos++;
+                }
+            }
+            printf("Esquerda\n");
+            cavalo++;
+            printf("%s: %d\n", (cavalo ==1) ? "Casa movida" : "Casas movidas", cavalo);
             break;
         case 0:
             printf("-----------------------------\n");
