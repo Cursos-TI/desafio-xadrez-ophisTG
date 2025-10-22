dd263347079cc660a5c0dd27e3032edc7952fa67
Rafael Ribeiro <163664337+ophisTG@users.noreply.github.com>
2025-10-22 20:28:47 +0000

    Desafio nvl Mestre - Adiciona recursividade

--- xadrez.c
+++ xadrez.c
@@ -2,6 +2,223 @@
 
 #include <stdio.h>
 
+
+// Recursividade da torre:
+void movimento_torre(int direcao, int casas_restante, int contagem){
+    // Define o encerramento:
+    if (casas_restante == 0) return;
+
+    // Imprime as casas:
+    switch (direcao){
+        case 1:
+            printf("Frente\n");
+            break;
+        case 2:
+            printf("Direita\n");
+            break;
+        case 3:
+            printf("Trás\n");
+            break;
+        case 4:
+            printf("Esquerda\n");
+            break;
+        default:
+            printf("Direção inválida\n");
+            return;
+        }
+    contagem++;
+    printf("%s: %d\n", (contagem == 1) ? "Casa movida" : "Casas movidas", contagem);
+
+    // Loop recursivo:
+    movimento_torre(direcao, casas_restante - 1, contagem);
+}
+
+// Define as escolhas do usuário para a peça:
+void peca_torre() {
+    int direcao, casas;
+    printf("Escolha a direção do movimento: \n");
+    printf("-----------------------------\n");
+    printf("[1] Frente\n");
+    printf("[2] Direita\n");
+    printf("[3] Trás\n");
+    printf("[4] Esquerda\n");
+    printf("Direção: ");
+    scanf("%d", &direcao);
+
+    printf("Quantas casas deseja mover: ");
+    scanf("%d", &casas);
+
+    movimento_torre(direcao, casas, 0);
+}
+
+// Recursividade do Bispo:
+void movimento_bispo(int direcao, int casas_restante, int contagem){
+    // Define saída:
+    if (casas_restante == 0) return;
+    
+    switch (direcao){
+            case 1:
+                // Loop externo vertical:
+                for (int i = 0; i < 1; i++){
+                    printf("Cima\n");
+                    // Loop interno diagonal:
+                    for (int direita = 0; direita < 1; direita++){
+                        printf("Direita\n");
+                    }
+                }
+                break;
+            case 2:
+                // Loop externo vertical:
+                for (int i = 0; i < 1; i++){
+                    printf("Cima\n");
+                    // Loop interno diagonal:
+                    for (int esquerda = 0; esquerda < 1; esquerda++){
+                        printf("Esquerda\n");
+                    }
+                }
+                break;
+            case 3:
+                // Loop externo vertical:
+                for (int i = 0; i < 1; i++){
+                    printf("Baixo\n");
+                    // Loop interno diagonal:
+                    for (int direita = 0; direita < 1; direita++){
+                        printf("Direita\n");
+                    }
+                }
+                break;
+            case 4:
+                // Loop externo vertical:
+                for (int i = 0; i < 1; i++){
+                    printf("Baixo\n");
+                    // Loop interno diagonal:
+                    for (int esquerda = 0; esquerda < 1; esquerda++){
+                        printf("Esquerda\n");
+                    }
+                }
+                break;
+            default:
+                printf("Direção inválida\n");
+                return;
+    }
+    contagem++;
+    printf("%s: %d\n", (contagem == 1) ? "Casa movida" : "Casas movidas", contagem);
+    // Loop recursivo:
+    movimento_bispo(direcao, casas_restante - 1, contagem);
+}
+
+// Define as escolhas do usuário para a peça:
+void peca_bispo() {
+    int direcao, casas;
+    printf("Escolha a direção do movimento: \n");
+    printf("-----------------------------\n");
+    printf("[1] Direita para cima\n");
+    printf("[2] Esquerda para cima\n");
+    printf("[3] Direita para baixo\n");
+    printf("[4] Esquerda para baixo\n");
+    printf("Direção: ");
+    scanf("%d", &direcao);
+
+    printf("Quantas casas deseja mover: ");
+    scanf("%d", &casas);
+
+    movimento_bispo(direcao, casas, 0);
+}
+
+// Recursividade da Rainha:
+void movimento_rainha(int direcao, int casas_restante, int contagem){
+    // Define saída:
+    if (casas_restante == 0) return;
+
+    switch (direcao){
+        // CASOS VERTICAIS E HORIZONTAIS:
+        case 1:
+            printf("Frente\n");
+            break;
+        case 2:
+            printf("Direita\n");
+            break;
+        case 3:
+            printf("Trás\n");
+            break;
+        case 4:
+            printf("Esquerda\n");
+            break;
+        // CASOS DIAGONAIS:
+        case 5:
+            // Loop externo vertical:
+            for (int i = 0; i < 1; i++){
+                printf("Cima\n");
+                // Loop interno diagonal:
+                for (int direita = 0; direita < 1; direita++){
+                    printf("Direita\n");
+                }
+            }
+            break;
+        case 6:
+            // Loop externo vertical:
+            for (int i = 0; i < 1; i++){
+                printf("Cima\n");
+                // Loop interno diagonal:
+                for (int esquerda = 0; esquerda < 1; esquerda++){
+                    printf("Esquerda\n");
+                }
+            }
+            break;
+        case 7:
+            // Loop externo vertical:
+            for (int i = 0; i < 1; i++){
+                printf("Baixo\n");
+                // Loop interno diagonal:
+                for (int direita = 0; direita < 1; direita++){
+                    printf("Direita\n");
+                }
+            }
+            break;
+        case 8:
+            // Loop externo vertical:
+            for (int i = 0; i < 1; i++){
+                printf("Baixo\n");
+                // Loop interno diagonal:
+                for (int esquerda = 0; esquerda < 1; esquerda++){
+                    printf("Esquerda\n");
+                }
+            }
+        default:
+            printf("Direção inválida\n");
+            return;
+    }
+    contagem++;
+    printf("%s: %d\n", (contagem == 1) ? "Casa movida" : "Casas movidas", contagem);
+    // Loop recursivo:
+    movimento_rainha(direcao, casas_restante - 1, contagem);
+}
+
+// Define as escolhas do usuário para a peça:
+void peca_rainha(){
+    int direcao, casas;
+    printf("Escolha a direção do movimento: \n");
+    printf("-----------------------------\n");
+    printf("Verticais e horizontais:\n");
+    printf("[1] Frente\n");
+    printf("[2] Direita\n");
+    printf("[3] Trás\n");
+    printf("[4] Esquerda\n");
+    printf("Diagonais:\n");
+    printf("[5] Direita para cima\n");
+    printf("[6] Esquerda para cima\n");
+    printf("[7] Direita para baixo\n");
+    printf("[8] Esquerda para baixo\n");
+    printf("Direção: ");
+    scanf("%d", &direcao);
+
+    printf("Quantas casas deseja mover: ");
+    scanf("%d", &casas);
+
+    movimento_rainha(direcao, casas, 0);
+
+}
+
 int main() {
     // Define e inicializa variaveis:
     int opcao, bispo = 1, rainha = 1, cavalo = 0;
@@ -28,34 +245,16 @@ int main() {
     // Estrutura de decisão baseada na escolha do jogador:
     switch(opcao){
         case 1:
-            //Movimenta a torre 5 casas para a direita:
-            printf("-----------------------------\n");
-            printf("Torre se move... \n");
-            for (int torre = 1; torre <=5; torre++){
-                printf("Direita\n");
-                printf("%s: %d\n", (torre ==1) ? "Casa movida" : "Casas movidas", torre);
-            }
+            //Movimenta a torre na vertical e horizontal:
+            peca_torre();
             break;
         case 2:
-            // Movimenta o bispo 5 casas na diagonal:
-            printf("Bispo se move...\n");
-            printf("-----------------------------\n");
-            while (bispo <= 5) {
-                printf("Cima\n");
-                printf("Direita\n");
-                printf("%s: %d\n", (bispo ==1) ? "Casa movida" : "Casas movidas", bispo);
-                bispo++;
-            }
+            // Movimenta o bispo na diagonal:
+            peca_bispo();
             break;
         case 3:
-            // Movimenta a rainha 8 casas para a esquerda
-            printf("-----------------------------\n");
-            printf("Rainha se move...\n");
-            do {
-                printf("Esquerda\n");
-                printf("%s: %d\n", (rainha ==1) ? "Casa movida" : "Casas movidas", rainha);
-                rainha++;
-            } while (rainha <= 8);
+            // Movimenta a rainha em qualquer direção:
+            peca_rainha();
             break;
         case 4:
             // Movimenta o cavalo em L para baixo e para esquerda:
@@ -64,13 +263,13 @@ int main() {
             for (int i = 0; i < 2; i++){
                 int passos = 0;
                 while (passos < 1){
-                    printf("Baixo\n");
+                    printf("Cima\n");
                     cavalo++;
                     printf("%s: %d\n", (cavalo ==1) ? "Casa movida" : "Casas movidas", cavalo);
                     passos++;
                 }
             }
-            printf("Esquerda\n");
+            printf("Direita\n");
             cavalo++;
             printf("%s: %d\n", (cavalo ==1) ? "Casa movida" : "Casas movidas", cavalo);
             break;
